2e47608bb797a0ead1225d0901bea8660186065e
Jordan <jtaillefer02@gmail.com>
2015-04-09 11:01:50 +0200

    uaich

--- main.cpp
+++ main.cpp
@@ -5,17 +5,12 @@
 #include "Processus.h"
 using namespace std;
 
-<<<<<<< HEAD
-int main(){
-    cout<<"Toto"<<endl;
-    return 0;
-=======
+
 int main()
-{	
+{
 	Processus procToTest(Noyau::creerProcessus(),new ELF(30000,50000,100000));
 	procToTest.afficher();
 	Processus procToTest2(Noyau::creerProcessus(),new ELF(30000,50000,100000));
 	procToTest2.afficher();
 	system("PAUSE");
->>>>>>> d03036c995f5fe02ff8d218c4a6ea25b1e0c6257
 }
