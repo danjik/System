8ddbe8278a09387153985b24d9a79e07b088015a
Spazzou <dropbox_wass@hotmail.fr>
2015-04-09 09:03:55 +0200

    Processus

--- Page.h
+++ Page.h
@@ -3,12 +3,20 @@ using namespace std;
 class Page
 {
 	static const int taillePage=4;
-	string adrDepart;
+	int adrDepart;
 	bool ecriture;
 	bool lecture;
 	bool execution;
 	//RESTE ADRESSE
 public:
+	int getAdrDepart(){return adrDepart;};
+	void setAdrDepart(int toSet){adrDepart=toSet;};
+	bool getEcriture(){return ecriture;};
+	void setEcriture(bool v){ecriture=v;};
+	bool getLecture(){return lecture;};
+	void setLecture(bool v){lecture=v;};
+	bool getExecution(){return execution;};
+	void setExecution(bool v){execution=v;};
 	Page(void);
 	~Page(void);
 };
